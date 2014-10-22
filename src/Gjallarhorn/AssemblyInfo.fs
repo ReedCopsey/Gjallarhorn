fa3d6507c727451b9303c0e8272a279b44f6d7c0
ReedCopsey <reed@reedcopsey.com>
2014-10-22 16:36:16 -0700

    Updated AssemblyINfo

--- src/Gjallarhorn/AssemblyInfo.fs
+++ src/Gjallarhorn/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Gjallarhorn")>]
 [<assembly: AssemblyProductAttribute("Gjallarhorn")>]
 [<assembly: AssemblyDescriptionAttribute("Framework for managing mutable data with change notification and live views")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.0.1"
