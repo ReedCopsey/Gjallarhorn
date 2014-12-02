1596aa7271d85157593ac97a2fa24d8248040b95
ReedCopsey <reed@reedcopsey.com>
2014-12-01 16:49:14 -0800

    Added Edit.mapConvertible
    For simple conversions between two IConvertible types

--- src/Gjallarhorn/Edit.fs
+++ src/Gjallarhorn/Edit.fs
@@ -7,6 +7,12 @@ open System
 
 /// Provides mechanisms for editing IMutatable instances
 module Edit =
-    /// Transforms a view value by using a specified mapping function.
+    /// Transforms a mutatable value for editing by using a specified mapping function for view and edit.
     let map (viewMapping : 'a -> 'b) (setMapping : 'b -> 'a) (provider : IMutatable<'a>) = 
         new MappingEditor<'a,'b>(provider, viewMapping, setMapping, false) :> IMutatable<'b>
+
+    /// Transforms a mutatable value from one IConvertible type to another.
+    let mapConvertible<'a,'b> (provider : IMutatable<'a>) =
+        let conv a : 'T = System.Convert.ChangeType(a, typeof<'T>) :?> 'T
+        let mut : IMutatable<'b> = map conv conv provider
+        mut
--- tests/Gjallarhorn.Tests/Edit.fs
+++ tests/Gjallarhorn.Tests/Edit.fs
@@ -9,10 +9,21 @@ open NUnit.Framework
 let ``Edit.map pushes proper edits through to source`` (start : 'a) (stView : string) finish (finishView : string) =
     let value = Mutable.create start
     let conv a : 'T = System.Convert.ChangeType(a, typeof<'T>) :?> 'T
-    let editor = Edit.map (fun (v : 'a) -> string(v)) (fun v -> conv v) value
+    let editor = Edit.map conv conv value
     Assert.AreEqual(box start, value.Value)
     Assert.AreEqual(stView, editor.Value)
 
     editor.Value <- finishView
     Assert.AreEqual(box finish, value.Value)
-    Assert.AreEqual(box finishView, editor.Value)
+    Assert.AreEqual(finishView, editor.Value)
+
+[<Test;TestCaseSource(typeof<Utilities>,"CasesStartEndToStringPairs")>]
+let ``Edit.mapConvertible pushes proper edits through to source`` (start : 'a) (stView : string) finish (finishView : string) =
+    let value = Mutable.create start
+    let editor = Edit.mapConvertible value
+    Assert.AreEqual(box start, value.Value)
+    Assert.AreEqual(stView, editor.Value)
+
+    editor.Value <- finishView
+    Assert.AreEqual(box finish, value.Value)
+    Assert.AreEqual(finishView, editor.Value)
