d01c8e84471920078f6366d334f2ff9dd81f56c5
ReedCopsey <reed@reedcopsey.com>
2016-01-11 13:19:00 -0800

    Updated project output for project scaffold support

--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -4,10 +4,12 @@
 #I "../../bin"
 
 (**
-Introducing your project
+Introducing Gjallarhorn
 ========================
 
-Say more
+Gjallarhorn is centered around two core types - Views and Mutables.
+
+A Mutable is a holder for mutable variables, similar to a reference cell.  They are typically created via Mutable.create:
 
 *)
 #r "Gjallarhorn.dll"
@@ -16,7 +18,11 @@ open Gjallarhorn
 // Create a mutable variable
 let variable = Mutable.create "Foo"
 printfn "hello = %s" variable.Value
+variable.Value <- "Bar"
+printfn "hello = %s" variable.Value
 
 (**
-TODO
+
+In this simple example, we show creating, reading from, and updating a Mutable.
+
 *)
