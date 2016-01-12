94cb310808e940c44c0c6f0c128813ab9ac30f61
ReedCopsey <reed@reedcopsey.com>
2016-01-11 17:27:49 -0800

    Updated Contributing in docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -53,13 +53,11 @@ API Reference
 Contributing and copyright
 --------------------------
 
-The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
-the project and submit pull requests. If you're adding a new public API, please also 
-consider adding [samples][content] that can be turned into a documentation. You might
-also want to read the [library design notes][readme] to understand how it works.
+The project is hosted on [GitHub][https://github.com/ReedCopsey/Gjallarhorn] where you can [report issues][https://github.com/ReedCopsey/Gjallarhorn/issues], fork 
+the project and submit pull requests. 
 
 The library is available under the Apache 2.0 License, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
-[License file][license] in the GitHub repository. 
+[License file][https://github.com/ReedCopsey/Gjallarhorn/blob/master/LICENSE.txt] in the GitHub repository. 
 
 *)
