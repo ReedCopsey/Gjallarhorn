645b745bd990a77eaf37ef152b3dff23d75477ff
ReedCopsey <reed@reedcopsey.com>
2016-01-14 13:17:49 -0800

    Reworked structure to allow reference docs to generate properly
    By creating a separate non-PCL project proxy for PCL files

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -1,9 +1,7 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
-#I "../../bin/Gjallarhorn"
-#I "../../bin/Gjallarhorn.Bindable"
-#I "../../bin/Gjallarhorn.Bindable.Wpf"
+#I "../../bin"
 #r "C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/v4.6.1/Facades/System.Runtime.dll"
 
 (**
---
+++ src/Gjallarhorn.ReferenceDocumentation/Readme.txt
@@ -0,0 +1,6 @@
+This project is merely linked in files from the other PCL projects.
+
+It exists purely to allow ProjectScaffold to build proper reference documentation, as
+the FSharp.Formatting routines fail on PCL libraries.
+
+All files from PCL projects should be linked into this project.
