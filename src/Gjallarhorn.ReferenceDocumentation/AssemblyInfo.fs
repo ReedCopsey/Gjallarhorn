dc863f76a2ae524bc9b7e12495c212e3fe079f26
ReedCopsey <reed@reedcopsey.com>
2016-01-14 17:06:21 -0800

    Update for assembly info generated by scaffolding

---
+++ src/Gjallarhorn.ReferenceDocumentation/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Gjallarhorn.ReferenceDocumentation")>]
+[<assembly: AssemblyProductAttribute("Gjallarhorn")>]
+[<assembly: AssemblyDescriptionAttribute("Framework for managing mutable data with change notification and live views")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
