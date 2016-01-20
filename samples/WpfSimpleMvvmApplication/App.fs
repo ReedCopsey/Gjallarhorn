710b240882b86d2dd9096edd8aca20425a78eaed
ReedCopsey <reed@reedcopsey.com>
2016-01-19 17:16:20 -0800

    Added comments

--- samples/WpfSimpleMvvmApplication/App.fs
+++ samples/WpfSimpleMvvmApplication/App.fs
@@ -7,6 +7,7 @@ type App = XAML<"App.xaml">
 [<STAThread>]
 [<EntryPoint>]
 let main _ = 
+    // Install WPF Binding targets
     Gjallarhorn.Wpf.install()
 
     let window = Views.MainWindow().Root
