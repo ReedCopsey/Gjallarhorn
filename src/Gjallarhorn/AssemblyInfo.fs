aefe8b69bf9e3bfd09f76b6fa7605d921d93fcc3
ReedCopsey <reed@reedcopsey.com>
2016-01-28 12:49:10 -0800

    Build update

--- src/Gjallarhorn/AssemblyInfo.fs
+++ src/Gjallarhorn/AssemblyInfo.fs
@@ -3,7 +3,7 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Gjallarhorn")>]
 [<assembly: AssemblyProductAttribute("Gjallarhorn")>]
-[<assembly: AssemblyDescriptionAttribute("Framework for managing mutable data with signaling")>]
+[<assembly: AssemblyDescriptionAttribute("Framework for managing mutable data with change notification and live views")>]
 [<assembly: AssemblyVersionAttribute("0.0.1")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
