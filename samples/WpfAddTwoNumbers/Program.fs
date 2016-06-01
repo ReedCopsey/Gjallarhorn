4790019182831e1902c2c7d413db199f7860503f
ReedCopsey <reed@reedcopsey.com>
2016-06-01 14:36:46 -0700

    Simplified "add two numbers" sample

--- samples/WpfAddTwoNumbers/Program.fs
+++ samples/WpfAddTwoNumbers/Program.fs
@@ -1,7 +1,6 @@
 ﻿open System
 
 open FsXaml
-open FsXaml.Wpf
 open System.Windows
 
 type MainWin = XAML<"MainWindow.xaml">
@@ -12,13 +11,6 @@ let main _ =
     Gjallarhorn.Wpf.install true |> ignore
 
     let app = Application()
-    let win = MainWin()
-
-    let context = Context.create { One = 42 ; Two = 54 }   
-    win.DataContext <- context
-
-    // This works because we created a binding subject, not a binding target.
-    // Subjects are observables of the "output" of the binding context
-    context.Add (fun v -> printfn "Value updated to %d" v)
+    let win = MainWin(DataContext = Context.create())
 
     app.Run win
