be7599671845827eaa4786c434fbe42b6e13d0d4
ReedCopsey <reed@reedcopsey.com>
2016-06-01 14:28:49 -0700

    Added new sample for adding two numbers

---
+++ samples/WpfAddTwoNumbers/Context.fs
@@ -0,0 +1,36 @@
+﻿module Context
+
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+
+type Values = { One : int ; Two : int }
+
+
+let create initialValue =
+    
+    // Create our binding subject
+    let subject =  Binding.createSubject ()
+
+    // Create two mutable values from our initial value
+    let one = Mutable.create initialValue.One
+    let two = Mutable.create initialValue.Two
+
+    // Bind these directly (two-way)
+    Binding.editDirect subject "Value1" noValidation one
+    Binding.editDirect subject "Value2" noValidation two
+
+    // Create a signal for our result
+    let result = Signal.map2 (+) one two
+    
+    // Display the results
+    subject.Watch "Result" result
+
+    // Set the results as our output (optional)
+    // Since we're doing this, the context acts as an observable stream of results
+    // If we used Binding.createTarget instead of createSubject, we would skip this 
+    // and not have an observable output
+    subject.OutputObservable result
+
+    // Return the binding subject
+    subject
---
+++ samples/WpfAddTwoNumbers/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+open FsXaml
+open FsXaml.Wpf
+open System.Windows
+
+type MainWin = XAML<"MainWindow.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =     
+    Gjallarhorn.Wpf.install true |> ignore
+
+    let app = Application()
+    let win = MainWin()
+
+    let context = Context.create { One = 42 ; Two = 54 }   
+    win.DataContext <- context
+
+    // This works because we created a binding subject, not a binding target.
+    // Subjects are observables of the "output" of the binding context
+    context.Add (fun v -> printfn "Value updated to %d" v)
+
+    app.Run win
