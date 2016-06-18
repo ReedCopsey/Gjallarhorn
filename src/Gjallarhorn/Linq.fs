2ab4e14ef2c57bd6f5b92d6e39abe91d0f76f230
Reed Copsey, Jr <reed@reedcopsey.com>
2016-06-18 12:48:09 -0700

    Initial setup of plumbing to implement C# API
    For core project

---
+++ src/Gjallarhorn/Linq.fs
@@ -0,0 +1,17 @@
+﻿namespace Gjallarhorn.Linq
+
+open Gjallarhorn
+open Gjallarhorn.Helpers
+open Gjallarhorn.Internal
+open Gjallarhorn.Validation
+
+open System
+open System.Runtime.CompilerServices
+
+[<AbstractClass;Sealed;Extension>]
+type SignalExtensions() =
+
+    [<Extension>]
+    static member Select<'a,'b> (this:ISignal<'a>, mapper:Func<'a,'b>) =
+        this
+        |> Signal.map mapper.Invoke 
---
+++ tests/Gjallarhorn.Linq.Tests/SignalLinq.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Gjallarhorn;
+using Gjallarhorn.Linq;
+
+namespace Gjallarhorn.Linq.Tests
+{
+    public class SignalLinq
+    {
+        System.Globalization.CultureInfo culture = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            culture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+        }
+
+        [Test]
+        public void SelectWorksOffSignal()
+        {
+            var value = MutableModule.Create(42);
+            var mapped = value.Select(v => (v + 2).ToString());
+
+            Assert.AreEqual(42, value.Value);
+            Assert.AreEqual("44", mapped.Value);
+        }
+    }
+}
