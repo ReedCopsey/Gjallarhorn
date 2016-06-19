6e210a9579ec16427d7bb1859c635ef4d85bf208
Reed Copsey, Jr <reed@reedcopsey.com>
2016-06-18 17:30:19 -0700

    Added SelectMany for C#

--- src/Gjallarhorn/Linq.fs
+++ src/Gjallarhorn/Linq.fs
@@ -6,6 +6,7 @@ open Gjallarhorn.Internal
 open Gjallarhorn.Validation
 
 open System
+open System.ComponentModel
 open System.Runtime.CompilerServices
 
 [<AbstractClass;Sealed>]
@@ -20,3 +21,12 @@ type SignalExtensions() =
     static member Select<'a,'b> (this:ISignal<'a>, mapper:Func<'a,'b>) =
         this
         |> Signal.map mapper.Invoke 
+                
+    [<Extension;EditorBrowsable(EditorBrowsableState.Never)>]
+    static member SelectMany<'a,'b>(this:ISignal<'a>, mapper:Func<'a,ISignal<'b>>) =
+        mapper.Invoke(this.Value)
+
+    [<Extension;EditorBrowsable(EditorBrowsableState.Never)>]
+    static member SelectMany<'a,'b,'c>(this:ISignal<'a>, mapper:Func<'a,ISignal<'b>>, selector:Func<'a,'b,'c>) : ISignal<'c> =
+        let b' = mapper.Invoke(this.Value)
+        Signal.map2 (fun a b -> selector.Invoke(a,b)) this b'
--- tests/Gjallarhorn.Linq.Tests/SignalLinq.cs
+++ tests/Gjallarhorn.Linq.Tests/SignalLinq.cs
@@ -46,5 +46,93 @@ namespace Gjallarhorn.Linq.Tests
             Assert.AreEqual(42, value.Value);
             Assert.AreEqual("44", mapped.Value);
         }
+
+        [Test]
+        public void SelectWorksWithQuerySyntax()
+        {
+            var value = Mutable.Create(42);
+            var mapped = from v in value
+                         let newV = v + 2
+                         select newV.ToString();
+
+            Assert.AreEqual(42, value.Value);
+            Assert.AreEqual("44", mapped.Value);
+        }
+
+        [Test]
+        public void SelectPropogatesChangesWithQuerySyntax()
+        {
+            var value = Mutable.Create(42);
+            var mapped = from v in value
+                         let newV = v + 2
+                         select newV.ToString();
+
+            Assert.AreEqual(42, value.Value);
+            Assert.AreEqual("44", mapped.Value);
+            value.Value = 55;
+
+            Assert.AreEqual(55, value.Value);
+            Assert.AreEqual("57", mapped.Value);
+        }
+
+        [Test]
+        public void SelectManyWorksWithQuerySyntax()
+        {
+            var value = Mutable.Create(42);
+            var sv = Mutable.Create("Foo");
+
+            var mapped = from v in value
+                         from s in sv
+                         let newV = s + v.ToString()
+                         select newV;
+
+            Assert.AreEqual(42, value.Value);
+            Assert.AreEqual("Foo42", mapped.Value);
+        }
+
+        [Test]
+        public void SelectManyWithFiveValuesWorksWithQuerySyntax()
+        {
+            var value1 = Mutable.Create(1);
+            var value2 = Mutable.Create("2");
+            var value3 = Mutable.Create(3);
+            var value4 = Mutable.Create(4);
+            var value5 = Mutable.Create("5");
+
+            var mapped = from a in value1
+                         from b in value2
+                         from c in value3
+                         from d in value4
+                         from e in value5
+                         select $"{a}{b}{c}{d}{e}";
+
+            Assert.AreEqual("12345", mapped.Value);
+            value3.Value = 0;
+            Assert.AreEqual("12045", mapped.Value);
+            value2.Value = "Foo";
+            value5.Value = "Bar";
+            Assert.AreEqual("1Foo04Bar", mapped.Value);
+        }
+
+        [Test]
+        public void SelectManyPropogatesChangesWithQuerySyntax()
+        {
+            var value = Mutable.Create(42);
+            var sv = Mutable.Create("Foo");
+
+            var mapped = from v in value
+                         from s in sv
+                         let newV = s + v.ToString()
+                         select newV;
+
+            Assert.AreEqual(42, value.Value);
+            Assert.AreEqual("Foo42", mapped.Value);
+
+            value.Value = 55;
+            sv.Value = "Bar";
+
+            Assert.AreEqual(55, value.Value);
+            Assert.AreEqual("Bar55", mapped.Value);
+        }
     }
 }
