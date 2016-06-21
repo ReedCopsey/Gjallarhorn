3463772ae2955260b846dbd860fc9fd94c19cede
s952163 <s995110@yahoo.co.jp>
2016-06-21 17:56:39 +0900

    Update validate_types.fsx
    Change API from View to Signal

--- docs/content/validate_types.fsx
+++ docs/content/validate_types.fsx
@@ -5,29 +5,34 @@
 #r "C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/v4.6.1/Facades/System.Runtime.dll"
 
 (**
-Validating Views and Mutables
+Validating Signals and Mutables
 ========================
 
-Views and Mutables in Gjallarhorn also provide a mechanism to filter based on validation rules.
-
+Signals and Mutables in Gjallarhorn also provide a mechanism to filter based on validation rules.
 A review of the [basics of validation in Gjallarhorn](validation.html) prior to reading this section will be beneficial.
+The validation engine in Gjallarhorn can be used to validate Signals and Mutables directly.
 
-The validation engine in Gjallarhorn can be used to validate Views and Mutables directly.
+`Signal.validate` allows you to filter a Signal into an `IValidatedSignal<'a,'a>`, which provides the following new properties:  
 
-`View.validate` allows you to filter a view into an `IValidatedView<'a>`, which provides two new properties:
-- `ValidationResult`: a `IView<ValidationResult>` with details of the validation of the current value of the view
+- `ValidationResult`: a `ISignal<ValidationResult>` with details of the validation of the current value of the view
+
 - `IsValid`: a boolean of whether the current view's value is valid
+
+- `RawInput`: a `ISignal<'a>` with the raw, unvalidated input
 
 *)
 
 #r "Gjallarhorn.dll"
+
 open Gjallarhorn
+
 open Gjallarhorn.Validation
+open Gjallarhorn.Validation.Validators
 
 let source = Mutable.create 0
 
-// Create a view on the mutable, validating it with 3 rules on the fly:
-let test = View.validate (notEqual 3 >> greaterThan 1 >> lessThan 5) source
+// Create a Signal on the mutable, validating it with 3 rules on the fly:
+let test = Signal.validate (notEqual 3 >> greaterThan 1 >> lessThan 5) source
 
 // Prints: Valid?: false
 printfn "Valid?: %b" test.IsValid
@@ -39,7 +44,8 @@ printfn "Valid?: %b" test.IsValid
 source.Value <- 3
 // Prints: Validation: Invalid ["Value cannot equal 3"]
 printfn "Validation: %A" test.ValidationResult.Value
-
+// Prints: Raw (unvalidated) input: 3
+printfn "Raw (unvalidated) input: %A" test.RawInput.Value
 
 (**
 
