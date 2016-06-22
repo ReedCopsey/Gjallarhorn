760e21ffbf62df504518ef669fd7f10d2fe1e0ef
ReedCopsey <reed@reedcopsey.com>
2016-06-22 14:13:05 -0700

    Restructured xamarin sample
    To be a bit cleaner

--- samples/Xamarin Forms/SimpleDataEntry/View.fs
+++ samples/Xamarin Forms/SimpleDataEntry/View.fs
@@ -1,12 +1,9 @@
 ﻿namespace SimpleDataEntry
 
-open System
-
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 
 open Gjallarhorn
-open Gjallarhorn.Bindable
 open Gjallarhorn.Validation
 
 type Editor() as this =
@@ -21,71 +18,28 @@ type Reporter() as this =
     do
         this.LoadFromXaml(typeof<Reporter>) |> ignore
 
-
 type Page1() as this = 
     inherit Xamarin.Forms.ContentPage()
     
     do
         this.LoadFromXaml(typeof<Page1>) |> ignore
 
-module VM =
-    // Create a source that reports values coming from an observable
-    let createReportingSource (initialValue : int) (updateStream : IObservable<int>) = 
-        let source = Binding.createSource()                        
-
-        // Take our initial + observable and turn it into a signal
-        let current = Signal.fromObservable initialValue updateStream
-        
-        // Output it to the view
-        Binding.toView source "Current" current
-
-        source
-
-    let createEditSource initialValue validation = 
-        let source = Binding.createObservableSource()        
-        
-        // Create a holder for current value
-        let currentValue = Mutable.create initialValue
-
-        // Bind the value to the view, and return a new signal 
-        // of the validated input from user
-        Binding.toFromViewConvertedValidated 
-            source 
-            "Current" 
-            string 
-            (Converters.stringToInt32 >> validation) // Back to int (for Xamarin), then validate
-            currentValue                
-        |> Observable.filterSome // Pass through valid options
-        |> source.OutputObservable  // Pipe this as our output directly
-
-        source
-
 type App() as self =
-    inherit Application()
-
-    let page = Page1()
+    inherit Application()    
 
     // These are being set here, just to demonstrate "defining once" and passing through system
     let initialValue = 12
+    
+    // Change validation rules here if desired...
     let validation = Validators.greaterThan 10 >> Validators.lessOrEqualTo 25
 
-    do 
-        let context = Binding.createSource()
+    // Create our context
+    let context = BindingContexts.createCompositeSource validation initialValue
+    
+    // Create our content page
+    let page = Page1(BindingContext = context)
 
-        // Create our edit source
-        // results implements IObservable<int>
-        let results = VM.createEditSource initialValue validation
-
-        // Create our reporting target
-        let reporting = VM.createReportingSource initialValue results
-
-        // Set it as our binding context
-        context |> Binding.constantToView "EditSource" results
-        
-        // Setup property for results
-        context |> Binding.constantToView "ResultsSource" reporting
-        
-        page.BindingContext <- context
+    do         
         self.MainPage <- page
 
     override __.OnStart() = ()
