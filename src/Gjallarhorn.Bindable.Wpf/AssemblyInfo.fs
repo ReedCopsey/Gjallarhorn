635062d6f526530378542da789e17a6476955487
JohanLarsson <johan_gavle1@hotmail.com>
2016-12-08 22:50:53 +0100

    Paket templates. Fix #25.

--- src/Gjallarhorn.Bindable.Wpf/AssemblyInfo.fs
+++ src/Gjallarhorn.Bindable.Wpf/AssemblyInfo.fs
@@ -6,6 +6,8 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("Framework for managing mutable data with change notification and live views")>]
 [<assembly: AssemblyVersionAttribute("0.0.5")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyCompanyAttribute("Reed Copsey, Jr.")>]
+[<assembly: AssemblyCopyright("Copyright 2016 Reed Copsey, Jr.")>]
 do ()
 
 module internal AssemblyVersionInformation =
