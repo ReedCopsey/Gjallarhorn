c216794766c3049231c2e0c99ff3527558e30c45
Jerold Haas <jeroldhaas@gmail.com>
2016-12-14 13:06:29 -0500

    add AtomicMutable<'a> to State.fs fixes #15 (#28)
    * add AtomicMutable<'a> to State.fs fixes #15
    
    * change deps from remote to local, Exchange instead of CompareExchange
    
    * cleanup
    
    * docs needed correction
    
    * rename mistake; will keep old name for now
    
    * add `Swap f`, remove spinlock, code reduction
    
    * change stored value to mutable; less boxing
    
    * rename AtomicMutable<'a> to InterlockMutable<'a>
    
    * add IAtomicMutable<'a> (close #29)
    
    * reimplement interfaces, rename to AtomicMutable
    
    * missing changes in last commit ^ :o

--- src/Gjallarhorn/State.fs
+++ src/Gjallarhorn/State.fs
@@ -1,6 +1,7 @@
 ﻿namespace Gjallarhorn
 
 open System
+open System.Threading
 open Gjallarhorn.Internal
 
 // The messages we allow for manipulation of our state
@@ -82,3 +83,42 @@ type State<'TModel,'TMsg> (initialState : 'TModel, update : 'TMsg -> 'TModel -> 
         member this.Dispose() = 
             (signal :?> System.IDisposable).Dispose()            
             GC.SuppressFinalize this
+
+/// A thread-safe wrapper using interlock for a mutable value with change notification
+type AtomicMutable<'a when 'a : not struct>(value : 'a) as self =
+    let mutable v = value
+    let deps = Dependencies.create [||] self
+    let rec swap (f : 'a -> 'a) =
+        let result = Interlocked.CompareExchange<'a>(&v, f v, v)
+        if obj.ReferenceEquals(result, v) then ()
+        else Thread.SpinWait 20; swap f
+    let setValue value =
+        let result = Interlocked.Exchange<'a>(&v, value)
+        deps.MarkDirty(this)
+
+    /// Gets and sets the Value contained within this mutable
+    member this.Value 
+        with get() = v
+        and set(value) = setValue(value)
+
+    interface IAtomicMutable<'a> with
+        member this.Swap f =
+            swap f
+            deps.MarkDirty(this)
+    interface System.IDisposable with
+        member this.Dispose() =
+            deps.RemoveAll(this)
+            GC.SuppressFinalize this
+    interface IObservable<'a> with
+        member this.Subscribe obs = deps.Subscribe(obs,this)
+    interface ITracksDependents with
+        member this.Track dep = deps.Add (dep,this)
+        member this.Untrack dep = deps.Remove (dep,this)
+    interface IDependent with
+        member this.UpdateDirtyFlag _ = ()
+        member this.HasDependencies with get() = deps.HasDependencies
+    interface IMutatable<'a> with
+        member this.Value with get() = v and set(newval) = setValue(newval)
+    interface ISignal<'a> with
+        member this.Value with get() = v
+
