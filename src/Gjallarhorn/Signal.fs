e375687902f124f7ccfc76a83d5ea87361e66cf7
Charles Roddie <charles.roddie@mathspire.com>
2021-01-03 14:13:03 +0000

    Remove unused inputs of lift and liftO

--- src/Gjallarhorn/Signal.fs
+++ src/Gjallarhorn/Signal.fs
@@ -118,7 +118,7 @@ module Signal =
         signal :> ISignal<'c>
 
     // Used to do mapN by lifting then mapping
-    let private lift f a b c =
+    let private lift f b c =
         let f' a bc = f a (fst bc) (snd bc)
         let bc = map2 (fun b c -> b,c) b c
         f', bc
@@ -131,42 +131,42 @@ module Signal =
     
     /// Combines three signals using a specified mapping function
     let map3 f v1 v2 v3 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map2 f1 v1 bc
     
     /// Combines four signals using a specified mapping function
     let map4 f v1 v2 v3 v4 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map3 f1 v1 bc v4
     
     /// Combines five signals using a specified mapping function
     let map5 f v1 v2 v3 v4 v5 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map4 f1 v1 bc v4 v5
         
     /// Combines six signals using a specified mapping function
     let map6 f v1 v2 v3 v4 v5 v6 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map5 f1 v1 bc v4 v5 v6
 
     /// Combines seven signals using a specified mapping function
     let map7 f v1 v2 v3 v4 v5 v6 v7= 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map6 f1 v1 bc v4 v5 v6 v7
 
     /// Combines eight signals using a specified mapping function
     let map8 f v1 v2 v3 v4 v5 v6 v7 v8 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map7 f1 v1 bc v4 v5 v6 v7 v8
 
     /// Combines nine signals using a specified mapping function
     let map9 f v1 v2 v3 v4 v5 v6 v7 v8 v9 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map8 f1 v1 bc v4 v5 v6 v7 v8 v9
 
     /// Combines ten signals using a specified mapping function
     let map10 f v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 = 
-        let f1, bc = lift f v1 v2 v3
+        let f1, bc = lift f v2 v3
         map9 f1 v1 bc v4 v5 v6 v7 v8 v9 v10
 
     /// Combines two signals of options, such as validation results, 
@@ -176,49 +176,49 @@ module Signal =
         map2 (optionMap2 mapping) provider1 provider2
 
     // Lift function for options
-    let private liftO f a b c =
+    let private liftO f b c =
         let f' a bc = f a (fst bc) (snd bc)
         let bc = mapOption2 (fun b c -> b,c) b c
         f', bc
 
     /// Combines three signals of options using a specified mapping function
     let mapOption3 f v1 v2 v3 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption2 f1 v1 bc
     
     /// Combines four signals of options using a specified mapping function
     let mapOption4 f v1 v2 v3 v4 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption3 f1 v1 bc v4
 
     /// Combines five signals of options using a specified mapping function
     let mapOption5 f v1 v2 v3 v4 v5 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption4 f1 v1 bc v4 v5
         
     /// Combines six signals of options using a specified mapping function
     let mapOption6 f v1 v2 v3 v4 v5 v6 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption5 f1 v1 bc v4 v5 v6
 
     /// Combines seven signals of options using a specified mapping function
     let mapOption7 f v1 v2 v3 v4 v5 v6 v7= 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption6 f1 v1 bc v4 v5 v6 v7
 
     /// Combines eight signals of options using a specified mapping function
     let mapOption8 f v1 v2 v3 v4 v5 v6 v7 v8 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption7 f1 v1 bc v4 v5 v6 v7 v8
 
     /// Combines nine signals of options using a specified mapping function
     let mapOption9 f v1 v2 v3 v4 v5 v6 v7 v8 v9 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption8 f1 v1 bc v4 v5 v6 v7 v8 v9
 
     /// Combines ten signals of options using a specified mapping function
     let mapOption10 f v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 = 
-        let f1, bc = liftO f v1 v2 v3
+        let f1, bc = liftO f v2 v3
         mapOption9 f1 v1 bc v4 v5 v6 v7 v8 v9 v10
 
     /// Filters the signal, so only values matching the predicate are cached and propogated onwards. 
